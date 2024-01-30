98493d33b0e2ca8a8a71f8ec22c054ac0d865d98
Shohanur-Shohan <shohanurshohan734@gmail.com>
2024-01-30 22:00:22 +0600

    api about all dynamic added

--- src/app/api/dashboard/member/all/route.js
+++ src/app/api/dashboard/member/all/route.js
@@ -2,6 +2,8 @@ import { PrismaClient } from "@prisma/client";
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(req, res){
 
     try{
